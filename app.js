fc515615c8a27a23893e2bc437af78fe94adbb35
himanshu <himanshumehta1114@gmail.com>
2017-09-07 23:58:14 +0530

    Added wikipedia Api

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
 	
 	$('#searchButton').on('click',function(){
 		var searchVal = document.getElementById('search').value;
-		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchVal + "&limit=10&namespace=0&format=json";
+		var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + searchVal;
+		var heading = "<h3>Here's the list of matching results : </h3>";
 		console.log("Url is : " + url);
+
+					
+		$.getJSON(url,function(data){
+			$('#results').append(heading);
+			$.each(data.query.pages,function(i){
+				$('#results').append("<h2><a href='https://en.wikipedia.org/?curid=" + data.query.pages[i].pageid + 
+                    			        "' target='_blank'>" + data.query.pages[i].title + "</a></h2>");
+                    			
+			});
+		});
 	});
 });
